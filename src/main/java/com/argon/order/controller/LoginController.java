9649250058b5b0b3f207216649af974ed4b7eb58
Argon <uon0707@naver.com>
2024-04-13 02:07:10 +0900

    @RequestParam 추가

--- src/main/java/com/argon/order/controller/LoginController.java
+++ src/main/java/com/argon/order/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -28,7 +29,7 @@ public class LoginController {
 
     @ResponseBody
     @PostMapping("/loginRestaurant")
-    public void loginRestaurant(String restaurantId, HttpSession session, Model model) {
+    public void loginRestaurant(@RequestParam(value="restaurantId") String restaurantId, HttpSession session) {
         session.setAttribute("loginRestaurantId", restaurantId);
     }
 
