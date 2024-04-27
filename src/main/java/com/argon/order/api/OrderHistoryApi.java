36c13ac90bd5bdf237029c772ef71b907d2f4464
argon <uon0707@naver.com>
2024-04-27 21:03:20 +0900

    주문내역 API 작업

--- src/main/java/com/argon/order/api/OrderHistoryApi.java
+++ src/main/java/com/argon/order/api/OrderHistoryApi.java
@@ -6,9 +6,7 @@ import com.argon.order.service.OrderHistoryMenuService;
 import com.argon.order.service.OrderHistoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,7 +20,7 @@ public class OrderHistoryApi {
     private final OrderHistoryMenuService orderHistoryMenuService;
 
     @PostMapping("/order-history")
-    public ResponseEntity<Object> orderHistory(OrderHistory orderHistory,int[] foodMenuNo, int[] orderCnt){
+    public ResponseEntity<Object> orderHistoryRegist(OrderHistory orderHistory,int[] foodMenuNo, int[] orderCnt){
         orderHistoryService.save(orderHistory);
         OrderHistoryMenu orderHistoryMenu;
         for(int i=0; i<foodMenuNo.length; i++){
@@ -32,6 +30,28 @@ public class OrderHistoryApi {
             orderHistoryMenu.setOrderCnt(orderCnt[i]);
             orderHistoryMenuService.save(orderHistoryMenu);
         }
+        return ResponseEntity.ok(orderHistoryMenuService);
+    }
+
+    @PutMapping("/order-history")
+    public ResponseEntity<Object> orderHistoryUpdate(OrderHistory orderHistory, int[] foodMenuNo, int[] orderCnt){
+        orderHistoryService.save(orderHistory);
+        OrderHistoryMenu orderHistoryMenu;
+        for(int i=0; i<foodMenuNo.length; i++){
+            orderHistoryMenu = new OrderHistoryMenu();
+            orderHistoryMenu.setOrderId(orderHistory.getOrderId());
+            orderHistoryMenu.setFoodMenuNo(foodMenuNo[i]);
+            orderHistoryMenu.setOrderCnt(orderCnt[i]);
+            orderHistoryMenuService.save(orderHistoryMenu);
+        }
+        return ResponseEntity.ok(orderHistoryMenuService);
+    }
+
+    @DeleteMapping("/order-history")
+    public ResponseEntity<Object> orderHistoryDelete(OrderHistory orderHistory){
+
+
+
         return ResponseEntity.ok(orderHistoryMenuService);
     }
 
