c7e022921fb1fabcdb9f8ae52e23becb4d6ea239
LEE-HYO-SANG\hanshin <uon0707@naver.com>
2024-05-14 11:34:53 +0900

    메인 redirect 방식 변경

--- src/main/java/com/argon/order/controller/MainController.java
+++ src/main/java/com/argon/order/controller/MainController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String index(){
-        return "redirect:/main";
-    }
-
     @GetMapping("/main")
     public String main(){return "admin/main";
     }
